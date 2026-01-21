d3150138a8855cf479fae70c342b8b44b0d18047
Baburao Adkane <b.g.adkane27@gmail.com>
2026-01-21 07:12:47 +0530

    config file added

---
+++ config/env.config.js
@@ -0,0 +1,5 @@
+export const ENV = {
+  baseUrl: process.env.BASE_URL || 'https://testerp.onenfinity.com/',
+  username: process.env.USERNAME || 'b4u@demo.com',
+  password: process.env.PASSWORD || '123'
+};
